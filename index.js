a567bedf0e8bccdf63d671bb8ce768f69030be08
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2021-11-18 14:25:23 +0200

    commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
 function rectg(r, h) {
     let x = r ** 2;
-    return x * Math.PI * h * 2;
+    return Math.PI * x * h;
 }
 
 console.log(rectg(5, 7));
@@ -30,9 +30,76 @@ console.log(rectg(5, 7));
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 
 
+// function parg (text) {
+//     document.write(`<p>${text}</p>`)
+//     document.write(`<p>${text}</p>`)
+// }
+// parg('texdt')
+
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+// function text (text){
+//     document.write(`<ul>`)
+//     document.write(`<li>${text}</li>`)
+//     document.write(`<li>${text}</li>`)
+//     document.write(`<li>${text}</li>`)
+//     document.write(`</ul>`)
+// }
+// text('список')
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+// Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+// function text(text) {
+//
+//     document.write(`<ul>`)
+//     for (i = 0; i < 3; i++) {
+//         document.write(`<li>${text}</li>`)
+//     }
+//     document.write(`</ul>`)
+//
+// }
+//
+// text('список')
+//
+
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+// function text(text) {
+//
+//
+//     let array=[false,14336,'text'];
+//     document.write(`<ul>`)
+//     for (i = 0; i < array.length; i++) {
+//         document.write(`<li>${array[i]}</li>`);
+//         document.write(`<li>${array[i]}</li>`);
+//         document.write(`<li>${array[i]}</li>`);
+//     }
+//     document.write(`</ul>`)
+// }
+//
+// text('список')
+
+
+
+// let Arr = [false,14336,'text'];
+// function array (arra){
+//     document.write(`<ul>`)
+//         for (let i = 0; i < 3; i++) {
+//             document.write(`<li>${Arr[i]}</li>`);
+//         }
+//     document.write(`</ul>`)
+//
+// }
+// array(array);
+
 //
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 
+let array = [{id: 1, name: 'Stas Grishun', age: 18}, {id: 3, name: 'Nosok', age: `120 years`}];
+function arr(array) {
+    for (let arrayelement of array) {
+        document.write(`<div>${arrayelement.id}.${arrayelement.name} - ${arrayelement.age}</div>`);
+    }
+}
+arr(array);
