fc01ff4505070788347e907028eb63548698bd1d
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2021-11-30 14:24:30 +0200

    commit

--- index.js
+++ index.js
@@ -13,15 +13,24 @@ id.innerText = "шо там?";
 
 // -- замініть текст параграфа з id 'rules' на будь-який інший
 
+idrules.innerText = "шо там?||part 2";
+
 // -- змініть кожному елементу колір фону на червоний
-
-
 // -- змініть кожному елементу колір тексту на синій
+let idrule = document.querySelectorAll('body> *');
 
+for (const idruleElement of idrule) {
+    idruleElement.style.background = 'red';
+    idruleElement.style.color = 'blue'; // спасибо, сочетание цветов отменное! глаза вытекли почти сразу! XD
+}
 
 // -- отримати весь список класів елемента з id=rules і вивести їх в console.log
-
-
+let classList = idrules.classList;
+console.log(classList);
 // -- поміняти колір тексту у всіх елементів fc_rules на червоний
 
+let clasrules = document.getElementsByClassName("fc_rules");
+for (const classListElement of clasrules) {
+    classListElement.style.color = 'red';
+}
 
