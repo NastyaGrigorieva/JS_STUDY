f2d60b881feaf432b36002de69eb09ee0ed69ca6
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2021-11-30 16:36:20 +0200

    commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 // 1) Напишіть код, який :
 // a) змінює класс тексту елемнту з ід r на назву групи в якій ви вчитесь (mon-year)
 
-let idR = document.getElementById("r");
-idR.classList.add('sep-21');
+// let idR = document.getElementById("r");
+// idR.classList.add('sep-21');
 
 // b) робить шириниу елементу ul 400px
-let ul = document.getElementsByTagName('ul');
-ul[0].style.width = '400px';
-ul[0].style.background = 'green';
+// let ul = document.getElementsByTagName('ul');
+// ul[0].style.width = '400px';
+// ul[0].style.background = 'green';
 
 // c) робить шириниу всіх елементів з класом linkList шириною 50%
 
@@ -23,8 +23,8 @@ ul[0].style.background = 'green';
 
 // d) отримує текст який зберігається в елементі з класом listElement2
 
-let listElement2 = document.getElementsByClassName('listElement2')[0];
-console.log(listElement2.innerText);
+// let listElement2 = document.getElementsByClassName('listElement2')[0];
+// console.log(listElement2.innerText);
 
 // e) отримує всі елементи li та змінює ім колір фону на сірий
 //
@@ -36,34 +36,65 @@ console.log(listElement2.innerText);
 
 // f) отримує всі елементи 'a' та додає їм клас anchor
 
-let a = document.getElementsByTagName('a');
-for (let aElement of a) {
-    aElement.classList.add('anchor');
-};
+// let a = document.getElementsByTagName('a');
+// for (let aElement of a) {
+//     aElement.classList.add('anchor');
+// };
 
 // g) отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
-let a = document.getElementsByTagName('a');
-for (let aElement of a){
-
-}
+// let a = document.getElementsByTagName('a');
+// for (let aElement of a){
+//  if (aElement.innerText === "link3"){
+//      aElement.style.fontSize = '40px';
+//  }
+// }
 
 // h) отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
+
+// let a = document.getElementsByTagName('a');
+// for (let aElement of a) {
+//     aElement.classList.add('element_XXX');
+//     let XXX = aElement.innerText
+// }
+// //?
 
 
 // i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
 
+// let subHeader = document.getElementsByClassName('sub-header');
+// for (let subHeaderElement of subHeader) {
+//     subHeader.style.background = prompt.innerText
+//     console.log(prompt('red'))
+// };
+//?
 
-// j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt(
-//
-// )
+
+// j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
+
+// let subHeader = document.getElementsByClassName('sub-header');
+// for (let subHeaderElement of subHeader) {
+//     subHeaderElement.style.background = 'blue';
+// }
+
+
 // k) отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
 
+// let content_1 = document.getElementsByClassName('content_1');
+// content_1.innerText = promt('mghfkcdtd');
 
 // l) отримати елементи p та змінити їм padding на 20px
-
+//  let p = document.getElementsByTagName('p');
+// for (let pElement of p) {
+//     pElement.style.padding = '20px'
+//
+// }
 
 // m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year. Пример sep-2021)
-
-
+//
+// let text2 = document.getElementsByClassName('text2');
+// for (let text2Element of text2) {
+//     text2Element.innerText = "шо там? sep-21"
+//
+// }
 
 
