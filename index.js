5f113a35f5aa24c669eeaf7f2ee6c1238364492e
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2021-11-30 15:58:09 +0200

    commit

---
+++ index.js
@@ -0,0 +1,69 @@
+// Взяти файл template_2.html та працювати в ньому
+// 1) Напишіть код, який :
+// a) змінює класс тексту елемнту з ід r на назву групи в якій ви вчитесь (mon-year)
+
+let idR = document.getElementById("r");
+idR.classList.add('sep-21');
+
+// b) робить шириниу елементу ul 400px
+let ul = document.getElementsByTagName('ul');
+ul[0].style.width = '400px';
+ul[0].style.background = 'green';
+
+// c) робить шириниу всіх елементів з класом linkList шириною 50%
+
+// let linkList = document.getElementsByClassName('linkList');
+// for (const item of linkList) {
+//     item.style.width = '50%';
+//     item.style.background = 'blue';
+// }
+
+
+
+
+// d) отримує текст який зберігається в елементі з класом listElement2
+
+let listElement2 = document.getElementsByClassName('listElement2')[0];
+console.log(listElement2.innerText);
+
+// e) отримує всі елементи li та змінює ім колір фону на сірий
+//
+// let li = document.querySelectorAll('li');
+// for (let liElement of li) {
+//     liElement.style.background = 'silver';
+// }
+
+
+// f) отримує всі елементи 'a' та додає їм клас anchor
+
+let a = document.getElementsByTagName('a');
+for (let aElement of a) {
+    aElement.classList.add('anchor');
+};
+
+// g) отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
+let a = document.getElementsByTagName('a');
+for (let aElement of a){
+
+}
+
+// h) отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
+
+
+// i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
+
+
+// j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt(
+//
+// )
+// k) отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
+
+
+// l) отримати елементи p та змінити їм padding на 20px
+
+
+// m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year. Пример sep-2021)
+
+
+
+
