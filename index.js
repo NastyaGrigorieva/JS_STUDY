05ea7222157a169027cbd4f5fcad9960843ff1db
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2021-12-06 14:51:08 +0200

    commit

--- index.js
+++ index.js
@@ -1,94 +1,69 @@
-// Все робити за допомоги js.
-// - створити блок,
-// const div = document.createElement('div');
-//     - додати йому класи wrap, collapse, alpha, beta
-// div.classList.add('wrap');
-// div.classList.add('collapce');
-// div.classList.add('alpha');
-// div.classList.add('beta');
-// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
-// div.style.padding = "20px";
-// div.style.margin = "5px";
-// div.style.background = "blue";
-// div.style.color = "gray";
-// // - додати цей блок в body.
-// document.body.appendChild(div);
-// - клонувати його повністю, та додати клон в body.
-//
-// document.body.appendChild(div.cloneNode(true));
+// - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
 
-// - Є масив:
-//     ['Main','Products','About us','Contacts']
-// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву
-// створює li та додає його до блоку .menu
-// Завдання робити через цикли.
+// const clik = document.getElementById('text');
+// const button = document.getElementById('button');
+// button.onclick = function () {
+//     clik.style.display = 'none';
+// }
 
-// const ul = document.getElementsByClassName('menu')[0];
-// const arrSrt = ['Main','Products','About us','Contacts'];
-// for (item of arrSrt) {
-//     const li = document.createElement('li');
-//     li.innerText = item;
-//     ul.appendChild(li);
-//
+//     - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+// const button = document.getElementById('button');
+// button.onclick = function () {
+//     button.style.display = 'none';
 // };
-//
-// - Є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
-// Завдання робити через цикли.
-//
-//  let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// for (const arrayElement of coursesAndDurationArray) {
-//     const div = document.createElement('div');
-//     const h2 = document.createElement('h2');
-//     h2.innerText = `${arrayElement.title} - ${arrayElement.monthDuration}`;
-//
-//     div.appendChild(h2);
-//     document.body.appendChild(div);
-
-// };;;;;;;;;;;; // это на всякий случай;
 
 
-//
-// - Є масив
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
-// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,
-// в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
-//     Завдання робити через цикли.
+// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.
+// При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
 
-for (const arrayElement of coursesAndDurationArray) {
+// document.getElementById('button').onclick = function () {
+//   const age =   document.getElementById('age').value;
+//     if (!age) {
+//         alert('а где собсна возраст');
+//     } else if (+age < 18) {
+//         alert('маловато будет');
+//     }else if (+age < 21) {
+// //         alert('староват');
+// //     } else {
+//         alert('Здарова');
+//     }
+// };
+
+
+// - Создайте меню, которое раскрывается/сворачивается при клике
+//
+// const menu = document.querySelector('.menu');
+// const title = menu.querySelector('.title');
+//
+// title.onclick = function () {
+//     menu.classList.toggle('hidden');
+// };
+
+
+// - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
+//     Вывести список комментариев в документ, каждый в своем блоке.
+//     Добавьте каждому комментарию по кнопке для сворачивания его body.
+
+const comments = [
+    {title: 'lorem', body: 'lorem ipsum dolo sit ameti'},
+    {title: 'lorem', body: 'lorem ipsum dolo sit ameti'},
+    {title: 'lorem', body: 'lorem ipsum dolo sit ameti'},
+]
+let divFather = document.createElement('div');
+for (const item of comments) {
     const div = document.createElement('div');
-    const h2 = document.createElement('h2');
+    const h3 = document.createElement('h3');
     const p = document.createElement('p');
+    const btn = document.createElement('button');
 
-    div.classList.add('item');
-    h2.classList.add('heading');
-    p.classList.add('description');
+    h3.innerText = item.title;
+    p.innerHTML = item.body;
+    btn.innerHTML = 'тыкнуть'
 
-    p.innerText = arrayElement.monthDuration;
-    h2.innerText = arrayElement.title;
-
-    div.append(h2, p);
-    document.body.append(div);
+    div.append(h3, p, btn);
+    divFather.append(div);
 }
+document.body.appendChild(divFather)
+
+
+
