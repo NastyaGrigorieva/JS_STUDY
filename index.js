6f4f14046ed284c7e16e6da316fb8e947b687fdc
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2021-12-07 13:30:13 +0200

    commit

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 // - створити 2 форми  по 2 інпути в кожній. ствоирити кнопку при кліку на яку считується
 // та виводиться на консоль інформація з цих 2х форм.
+//
+// const div1 = document.createElement('div');
+// const form1 = document.createElement('form');
+// const div2 = document.createElement('div')
+// const form2 = document.createElement('form');
+// const input1 = document.createElement('input');
+// const input2 = document.createElement('input');
+// const input3 = document.createElement('input');
+// const input4 = document.createElement('input');
+// const button1 = document.createElement('button');
+// const button2 = document.createElement('button');
+//
+// form1.setAttribute('name', 'form1')
+// form2.setAttribute('name', 'form2')
+//
+// button1.innerText = 'кнопачка 1';
+// button2.innerText = 'кнопачка 2';
+//
+//
+// document.body.appendChild(div1);
+// document.body.appendChild(div2);
+// div1.appendChild(form1);
+// form1.appendChild(input1);
+// form1.appendChild(input3);
+// form1.appendChild(button1);
+// div2.appendChild(form2);
+// form2.appendChild(input2);
+// form2.appendChild(input4);
+// form2.appendChild(button2);
+//
+//
+// button1.addEventListener('click', function (e){
+//     e.preventDefault()
+//     console.log(document.forms.form1[0].value);
+//     console.log(document.forms.form1[1].value);
+//
+// });
+// button2.addEventListener('click', function (event){
+//     event.preventDefault()
+//     console.log(document.forms.form2[0].value);
+//     console.log(document.forms.form2[1].value);
+//
+// });
+
+
 
 
 //     Кнопка повинна лежати за межами форм (Щоб ьуникнути  перезавантаження сторінки)
 // Доступ до інпутів через Forms API. Отже дайте формі та інпутам всі необхідні атрибути.
-
-
 
 
 // - Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
@@ -13,14 +56,11 @@
 // (Додатковачастина для завдання)
 
 
-
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
 //     Якщо людина вводить слово і воно міститься в масиві не цензурних слів
 // кинути алерт з попередженням.
 //     Перевірку робити при натисканні на кнопку
-
-
 
 
 // - Сворити масив не цензцрних слів.
