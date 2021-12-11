1cb012f617cb05b1fa2e8a455457680ac6a6cd8f
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2021-12-11 12:32:41 +0200

    commit

--- index.js
+++ index.js
@@ -1,94 +1,41 @@
-// Все робити за допомоги js.
-// - створити блок,
-// const div = document.createElement('div');
-//     - додати йому класи wrap, collapse, alpha, beta
-// div.classList.add('wrap');
-// div.classList.add('collapce');
-// div.classList.add('alpha');
-// div.classList.add('beta');
-// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
-// div.style.padding = "20px";
-// div.style.margin = "5px";
-// div.style.background = "blue";
-// div.style.color = "gray";
-// // - додати цей блок в body.
-// document.body.appendChild(div);
-// - клонувати його повністю, та додати клон в body.
-//
-// document.body.appendChild(div.cloneNode(true));
+// -створити форму з інпутами для name та age.
+//     При відправці форми записати об'єкт в localstorage
 
-// - Є масив:
-//     ['Main','Products','About us','Contacts']
-// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву
-// створює li та додає його до блоку .menu
-// Завдання робити через цикли.
-
-// const ul = document.getElementsByClassName('menu')[0];
-// const arrSrt = ['Main','Products','About us','Contacts'];
-// for (item of arrSrt) {
-//     const li = document.createElement('li');
-//     li.innerText = item;
-//     ul.appendChild(li);
+// const name = document.forms.user.name;
+// const age = document.forms.user.age;
+// const button = document.getElementById('button');
+// const key = 'key';
 //
+// const form = (name, age) => {
+//    const user = {
+//         name: name,
+//         age: age
+//     };
+//     localStorage.setItem(key, JSON.stringify(user));
 // };
 //
-// - Є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
-// Завдання робити через цикли.
-//
-//  let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// for (const arrayElement of coursesAndDurationArray) {
-//     const div = document.createElement('div');
-//     const h2 = document.createElement('h2');
-//     h2.innerText = `${arrayElement.title} - ${arrayElement.monthDuration}`;
-//
-//     div.appendChild(h2);
-//     document.body.appendChild(div);
-
-// };;;;;;;;;;;; // это на всякий случай;
+// button.onclick = () => {
+//     form(name.value, age.value);
+// };
 
 
-//
-// - Є масив
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
-// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,
-// в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
-//     Завдання робити через цикли.
 
-for (const arrayElement of coursesAndDurationArray) {
-    const div = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const p = document.createElement('p');
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
 
-    div.classList.add('item');
-    h2.classList.add('heading');
-    p.classList.add('description');
+const model = document.forms.car.model;
+const type = document.forms.car.type;
+const volume = document.forms.car.volume;
+const button2 = document.getElementById('button');
+const item = 'keyCar';
 
-    p.innerText = arrayElement.monthDuration;
-    h2.innerText = arrayElement.title;
+const form = (modelCar, typeCar, volumeCar) => {
+    const arr = JSON.parse(localStorage.getItem(item)) || [];
 
-    div.append(h2, p);
-    document.body.append(div);
-}
+    arr.push({modelCar, typeCar, volumeCar})
+    localStorage.setItem(item, JSON.stringify(arr));
+};
+
+button2.onclick = () => {
+    form(model.value, type.value, volume.value);
+};
