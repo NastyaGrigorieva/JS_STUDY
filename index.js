1838883e541555f2a94d700c3ddf807c1a98f34a
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2021-12-16 17:56:44 +0200

    commit

--- index.js
+++ index.js
@@ -1,94 +1,212 @@
-// Все робити за допомоги js.
-// - створити блок,
-// const div = document.createElement('div');
-//     - додати йому класи wrap, collapse, alpha, beta
-// div.classList.add('wrap');
-// div.classList.add('collapce');
-// div.classList.add('alpha');
-// div.classList.add('beta');
-// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
-// div.style.padding = "20px";
-// div.style.margin = "5px";
-// div.style.background = "blue";
-// div.style.color = "gray";
-// // - додати цей блок в body.
-// document.body.appendChild(div);
-// - клонувати його повністю, та додати клон в body.
-//
-// document.body.appendChild(div.cloneNode(true));
+// TODO HW
+// Зробити свій розпорядок дня. // // У вас має бути більше 10 асинхронних дій з рандомними затримками.
+// Вам необхідно синхронізувати всі свої дії за допомогою промісів та async await (Код має бути написаний окремо)
+// Затримка має бути НЕ в порядку зростання, а будь яка. При тому ваші дії мають бути синхронізовані.
+// // Напиклад. // Прикнутись - 0.3с // Поснідати - 1с // Піти в душ - 0.5с // Дочекатись автобус - 3с // Пообідати - 1с // // І так далі
 
-// - Є масив:
-//     ['Main','Products','About us','Contacts']
-// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву
-// створює li та додає його до блоку .menu
-// Завдання робити через цикли.
 
-// const ul = document.getElementsByClassName('menu')[0];
-// const arrSrt = ['Main','Products','About us','Contacts'];
-// for (item of arrSrt) {
-//     const li = document.createElement('li');
-//     li.innerText = item;
-//     ul.appendChild(li);
 //
+// function wakeUp(randomNum) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (randomNum > 0.25) {
+//                 resolve('Здаровасики');
+//             }
+//             reject('Я памэрла');
+//         }, 1000)
+//     });
+// }
+//
+// function brushTooth(randomNum) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (randomNum > 0.20) {
+//                 resolve('Чищу зубки');
+//             }
+//             reject('Я памэрло');
+//         }, 3000)
+//     });
+// }
+//
+// function brushPerushki(randomNum) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (randomNum > 0.25) {
+//                 resolve('Очистилась');
+//             }
+//             reject('Грязная тварь');
+//         }, 500);
+//     });
+// }
+//
+// function brushHard(randomNum) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (randomNum > 0.25) {
+//                 resolve('ВЫмой всю хату');
+//             }
+//             reject('Грязная тварь');
+//         }, 10000)
+//     });
+// }
+//
+// function coocingHard(randomNum) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (randomNum > 0.25) {
+//                 resolve('Наготовь еды на месяц за пару часов');
+//             }
+//             reject('Ленивая тварь');
+//         }, 300);
+//     });
+// }
+//
+// function washTheWindows(randomNum) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (randomNum > 0.25) {
+//                 resolve('Вымой все окна');
+//             }
+//             reject('Выпала,упс');
+//         }, 100)
+//     });
+// }
+//
+// function tigers(randomNum) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (randomNum > 0.25) {
+//                 resolve('Отгрызи когти тигру');
+//
+//             }
+//             reject('Я памерэло');
+//         }, 1000)
+//     });
+// }
+
+// const myDayList = async () => {
+//     try{
+//         const a = await wakeUp(Math.random());
+//         console.log(a);
+//         const b = await brushTooth(Math.random());
+//         console.log(b);
+//         const c = await brushPerushki(Math.random());
+//         console.log(c);
+//         const d = await brushHard(Math.random());
+//         console.log(d);
+//         const e = await coocingHard(Math.random());
+//         console.log(e);
+//         const f = await washTheWindows(Math.random());
+//         console.log(f);
+//         const g = await tigers(Math.random());
+//         console.log(g);
+//
+//
+//     } catch (e) {
+//         console.error(e)
+//     }
 // };
 //
-// - Є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
-// Завдання робити через цикли.
+// myDayList()
+
+
+// wakeUp(Math.random())
+//     .then(resp => {
+//         console.log(resp)
+//         return brushTooth(Math.random());
+//     })
 //
-//  let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// for (const arrayElement of coursesAndDurationArray) {
-//     const div = document.createElement('div');
-//     const h2 = document.createElement('h2');
-//     h2.innerText = `${arrayElement.title} - ${arrayElement.monthDuration}`;
+//     .then(resp => {
+//         console.log(resp)
+//         return brushPerushki(Math.random());
+//     })
+//     .then(resp => {
+//         console.log(resp)
+//         return brushHard(Math.random());
+//     })
+//     .then(resp => {
+//         console.log(resp)
+//         return coocingHard(Math.random());
+//     })
+//     .then(resp => {
+//         console.log(resp)
+//         return washTheWindows(Math.random());
+//     })
+//     .then(resp => {
+//         console.log(resp)
+//         return tigers(Math.random());
+//     })
 //
-//     div.appendChild(h2);
-//     document.body.appendChild(div);
+// .then(resp => console.log(resp))
+// .catch(e => console.error(e))
+// .finally(() => console.log('Финита ля комедиа'))
 
-// };;;;;;;;;;;; // это на всякий случай;
 
-
+// function wakeUp(randomNum, callback) {
+//     setTimeout(() => {
+//         if (randomNum > 0.25) {
+//             console.log('Дратути');
+//             return callback(Math.random(), brushPerushki)
+//         }
+//         console.log('Я памерэло');
+//     }, 2000)
+// }
 //
-// - Є масив
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
-// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,
-// в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
-//     Завдання робити через цикли.
+// function brushPerushki(randomNum, callback) {
+//     setTimeout(() => {
+//         if (randomNum > 0.25) {
+//             console.log('Чищу зубки');
+//             return callback(Math.random(), brushHard)
+//         }
+//         console.log('Я памерэло');
+//     }, 200)
+// }
+// wakeUp(Math.random(), brushPerushki)
+//
+//
+// function brushHard(randomNum, callback) {
+//     setTimeout(() => {
+//         if (randomNum > 0.25) {
+//             console.log('Вымой всю хату');
+//             return callback(Math.random(),coocingHard)
+//         }
+//         console.log('Я памерэло');
+//     }, 3000)
+// }
+//
+// function coocingHard(randomNum, callback) {
+//     setTimeout(() => {
+//         if (randomNum > 0.25) {
+//             console.log('Наготовь еды на месяц за пару часов');
+//             return callback(Math.random(),washTheWindows)
+//         }
+//         console.log('Ленивая тварь');
+//     }, 10000)
+// }
+//
+// function washTheWindows(randomNum, callback) {
+//     setTimeout(() => {
+//         if (randomNum > 0.25) {
+//             console.log('Отгрызи когти тигру');
+//             return callback(Math.random(),tigers)
+//         }
+//         console.log('Я Выпала,упс');
+//     }, 1000)
+// }
+//
+// function tigers(randomNum) {
+//     setTimeout(() => {
+//         if (randomNum > 0.25) {
+//             console.log('Отгрызи когти тигру');
+//         }
+//         console.log('Я памерэло');
+//     }, 500)
+// }
+//
+//
+//
 
-for (const arrayElement of coursesAndDurationArray) {
-    const div = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const p = document.createElement('p');
 
-    div.classList.add('item');
-    h2.classList.add('heading');
-    p.classList.add('description');
 
-    p.innerText = arrayElement.monthDuration;
-    h2.innerText = arrayElement.title;
 
-    div.append(h2, p);
-    document.body.append(div);
-}
+
